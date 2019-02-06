2a7151391c343de90e43df53b5368ed0551db1cb
kuniyoshi1 <knuiyosi111@gmail.com>
2019-02-06 13:09:36 +0900

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,43 @@
 //格納用の配列
-let data = {
-  task:[],
-  done:[]
-}
+let data;
 
 //アイコンをここに用意します
 let removeIcon = '<i class="fas fa-trash-alt fa-lg"></i>';
 let doneIcon = '<i class="far fa-check-circle"></i>';
+
+
+//localstorageに保存させる為のコード
+
+//読み込まれたタイミングで、ローカルストレージを用意するもしくは、配列を用意する。
+
+document.addEventListener('DOMContentLoaded', function(){
+  
+  //もし、データがlocalstorageの中に保存されていたら
+  if(localStorage.getItem('todoList')){
+      
+    //data配列にローカルストレージの内容を格納してちょ
+      data = JSON.parse(localStorage.getItem('todoList'));
+    
+    //データの表示
+    renderTodoList();
+    
+     }else{
+       
+       //data配列にtaskキーとdoneキーを作ってくれよ
+       data = {
+         task:[],
+         done:[]
+       };
+     
+     }
+  
+  localStorage.setItem('todoList',JSON.stringify(data));
+  
+  
+});
+
+
+
 
 
 
@@ -28,6 +59,9 @@ function addTask(value){
   //データ(valueの中身)を配列に格納
   data.task.push(value);
   addTaskToDOM(value);
+  
+  //ローカルストレージのアップデート
+  dataObjectUpdated();
 
 }
 
@@ -74,6 +108,9 @@ function addTaskToDOM(text, isDone){
   done.classList.add('done');
   //htmlに追加
   done.innerHTML = doneIcon;
+  //押された時の処理
+  done.addEventListener('click',doneTask);
+  
 
   
   
@@ -91,6 +128,7 @@ function addTaskToDOM(text, isDone){
 
 function removeTask(){
   let task = this.parentNode.parentNode;
+//  console.log(task);
   let id = task.parentNode.id;
   let value = task.textContent;
   
@@ -99,18 +137,94 @@ function removeTask(){
   
   //配列から削除
   if(id === 'not-yet'){
+    //indexOfで配列の番号を取得して、splice(〇〇,1)で１つだけデータ削除
     data.task.splice(data.task.indexOf(value), 1);
   }else{
     data.done.splice(data.done.indexOf(value), 1);
    }
   
+  //ローカルストレージのアップデート
+  dataObjectUpdated();
+  
 }
 
 
 function doneTask(){
+  let task = this.parentNode.parentNode;
+  let id = task.parentNode.id;
   
+  if(id !== 'not-yet'){
+    return;
+  }
+  
+  let value = task.textContent;
+  
+  //完了一覧に追加するコード
+  let target = document.getElementById('done');
+  
+  target.insertBefore(task,target.childNodes[0]);
+  
+  //配列更新
+  data.task.splice(data.task.indexOf(value),1);
+  data.done.push(value);
+  
+  //ローカルストレージのアップデート
+  dataObjectUpdated();
   
 }
+
+
+
+//ローカルストレージのデータをアップデートする為のコード
+function dataObjectUpdated(){
+  localStorage.setItem('todoList',JSON.stringify(data));
+  
+  console.log(localStorage);
+}
+
+
+//ローカルストレージの内容を表示させる為のコード
+function renderTodoList(){
+  
+  //未完了タスクを一覧で表示
+  for(let value of data.task){
+    
+    addTaskToDOM(value);
+  
+  }
+
+  
+  //完了タスクを一覧で表示
+  for(let value of data.done){
+    addTaskToDOM(value,true);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
